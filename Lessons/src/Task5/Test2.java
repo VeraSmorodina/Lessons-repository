21bc5a1c36508c4cd1bc779933f99493389a6c8b
Sergei Krainyukov <sk-new@mail.ru>
2023-12-13 15:07:41 +0300

    Task12

--- Lessons/src/Task5/Test2.java
+++ Lessons/src/Task5/Test2.java
@@ -1,12 +1,16 @@
 package Task5;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Test2 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Введите ваш возраст: ");
-        if (scanner.nextInt() < 18) {
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        Logger.getAnonymousLogger().info("log: "+age);
+        assert age <= 120;
+        if (age < 18) {
             System.out.println("Вы несовершеннолетний");
         } else {
             System.out.println("Вы совершеннолетний");
