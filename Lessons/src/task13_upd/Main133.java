3db4498d0adc4a7711a515ace72a39dba3ab8189
Sergei Krainyukov <sk-new@mail.ru>
2024-01-17 16:10:56 +0300

    Task13_upd

--- Lessons/src/task13_upd/Main133.java
+++ Lessons/src/task13_upd/Main133.java
@@ -1,4 +1,42 @@
 package task13_upd;
 
 public class Main133 {
+    public static void sort1(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Обмен элементов местами
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+    public static void assertSorted(int[] array) throws AssertionError {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                throw new AssertionError("Массив не отсортирован: элемент на позиции " + i + " больше элемента на позиции " + (i + 1));
+            }
+        }
+    }
+
+    public static void printArray(int[] array2) {
+        System.out.print("Отсортированный массив: ");
+        for (int value : array2) {
+            System.out.print(value + " ");
+        }
+    }
+
+    public static boolean arrayEquals(int[] array1, int[] array2) {
+        if (array1.length != array2.length)
+            return false;
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] != array2[i])
+                return false;
+        }
+        return true;
+    }
 }
--- Lessons/test/Main13Test.java
+++ Lessons/test/Main13Test.java
@@ -1,2 +1,55 @@
+import Task13.Main;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
 public class Main13Test {
+    @Test
+    public void test1() {
+        //Первый вид теста - белый ящик
+        System.out.println("Пример 1");
+        int[] array = {10, 2, 8, 6, 7, 3, 5, 4, 9, 1};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Main.sort1(array);
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void test2() {
+        //Второй вид теста - черный ящик
+        System.out.println("Пример 2");
+        int[] testArray = {10, 2, 8, 6, 7, 3, 5, 4, 9, 1};
+        int[] testArray1 = {10, 2, 8, 6, 7, 3, 5, 4, 9, 1};
+        Main.sort1(testArray);
+        Arrays.sort(testArray1);
+        assertArrayEquals(testArray1, testArray);//проверяем одинаково ли сортирует массив мой метод и метод из стандартной библиотеки
+    }
+
+    @Test
+    public void test3() {
+        int[] testArray = {10, 2, 8, 6, 7, 3, 5, 4, 9, 1};
+        int[] testArray1 = {10, 2, 8, 6, 7, 3, 5, 4, 9, 1};
+        boolean a = Main.arrayEquals(testArray, testArray1);
+        boolean b = Arrays.equals(testArray, testArray1);
+        assertEquals(b, a);
+    }
+
+    @Test
+    public void test4() {
+        int[] sortedArray = {1, 2, 3, 4, 5};
+        int[] unsortedArray = {5, 4, 3, 2, 1};
+        try {
+            Main.assertSorted(sortedArray);
+        } catch (AssertionError e) {
+            fail("Метод выбросил исключение для отсортированного массива: " + e.getMessage());
+        }
+        try {
+            Main.assertSorted(unsortedArray);
+            fail("Метод не выбросил исключение для неотсортированного массива");
+        } catch (AssertionError e) {
+            assertEquals("Массив не отсортирован: элемент на позиции 0 больше элемента на позиции 1", e.getMessage());
+        }
+    }
 }
